81b17dc9f57c2a735e3aecace56c9f864f2cf834
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-04-22 10:54:40 +0200

    update database connection configuration to use environment variables and add connection success logging

--- data/db.js
+++ data/db.js
@@ -4,8 +4,18 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: "movies_db",
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Errore di connessione:", err);
+  } else {
+    console.log("Connessione riuscita");
+    connection.release();
+  }
 });
 
 module.exports = pool;
