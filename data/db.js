b367a7644f26e004b9754c66dee2f5d6ad2489ce
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-04-22 10:03:57 +0200

    establish database connection using mysql2 and create connection pool

---
+++ data/db.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "movies_db",
+  waitForConnections: true,
+});
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
+const db = require("./data/db");
 
+const app = express();
 app.use(express.json());
+
+db.getConnection((err, conn) => {
+  if (err) console.error("DB connection error:", err);
+  else {
+    console.log("DB connected");
+    conn.release();
+  }
+});
 
 const port = process.env.PORT || 3000;
 
